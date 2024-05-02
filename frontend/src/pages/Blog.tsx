c5a41337664c3e9e2c804123ad99ea173ed8aadd
KDVaibhav <va21032002@gmail.com>
2024-05-02 13:56:34 +0530

    complete

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,14 +4,14 @@ import { FullBlog } from "../component/FullBlog";
 import { Spinner } from "../component/Spinner";
 import { useBlog } from "../hooks/index";
 import { useParams } from "react-router-dom";
-
+// import { Blog } from "../hooks/index";
  const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({
     id: id||""
   }
   );
-  if (loading) {
+  if (loading || !blog) {
     return <div>
       <Appbar/>
       <div className="h-screen flex flex-col justify-center">
