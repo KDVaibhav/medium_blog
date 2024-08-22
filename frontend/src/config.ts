ec88234836bab67a6a3c236c1debbb19753ba15e
KDVaibhav <va21032002@gmail.com>
2024-08-22 21:07:32 +0530

    medium-blog-clone-done

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const BACKEND_URL = "https://medium_blog_backend.va21032002.workers.dev/";
-=======
-export const BACKEND_URL = "https://week-13-offline.va21032002.workers.dev";
->>>>>>> 93334c2f4359a2105afedc9048aa5eeb525d8be7
